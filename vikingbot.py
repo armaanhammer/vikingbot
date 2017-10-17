33658c2bf6b5d915bbd23929fb6210fa8508f794
sandeep <cvs.sandeep@gmail.com>
2017-10-17 02:24:26 +0000

    ADDED speech output for zebra Bluettoth

--- vikingbot.py
+++ vikingbot.py
@@ -1,6 +1,7 @@
 import motor_controller as MC
 import ultrasonic as US
 import RPi.GPIO as GPIO
+import subprocess
 
 GPIO.setmode(GPIO.BCM)
 vikingbotMotors = MC.MotorController()
@@ -13,6 +14,7 @@ vikingbotMotors.setup_PWM()
 vikingbotMotors.start_PWM()
 vikingbotMotors.set_motorSpeed(90,90)
 vikingbotMotors.set_SleepTime(2)
+subprocess.call(["espeak","-s 120 -v en ", "Zebra is moving now"] , stdout=None, stderr=subprocess.STDOUT)
 vikingbotMotors.goForward()
 vikingbotMotors.set_SleepTime(1)
 vikingbotMotors.turnLeft()
@@ -22,7 +24,7 @@ vikingbotMotors.set_SleepTime(2)
 vikingbotMotors.goBack()
 
 #ultrasonicSensorBack.setup_GPIO()
-
+#subprocess.call(["espeak","-s 120 -v en ", "Intoduction to robotics"] , stdout=None, stderr=subprocess.STDOUT)
 #while(True):
 
 #        if (ultrasonicSensorBack.get_distance() > 10):
