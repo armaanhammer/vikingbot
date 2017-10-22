9e7dc67cfbaa03790bd2da006835f02693b64e46
Schnelled <schnelle@pdx.edu>
2017-10-22 19:01:37 +0000

    Added comments

--- vikingbot.py
+++ vikingbot.py
@@ -3,18 +3,27 @@ import ultrasonic as US
 import RPi.GPIO as GPIO
 import subprocess
 
+#set GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
+
+# create objects for motor controller and distance sensor
 vikingbotMotors = MC.MotorController()
 ultrasonicSensorBack = US.Ultrasonic()
 
 #GPIO.cleanup()
 
+# setup the motors
 vikingbotMotors.setup_GPIO(1,0)
+
+# setup and start PWM set the dulty cycles to 90
 vikingbotMotors.setup_PWM()
 vikingbotMotors.start_PWM()
 vikingbotMotors.set_motorSpeed(90,90)
+
+# set the delay between motions to 2 seconds
 vikingbotMotors.set_SleepTime(2)
-subprocess.call(["espeak","-s 120 -v en ", "Zebra is moving now"] , stdout=None, stderr=subprocess.STDOUT)
+
+#Test the movements
 vikingbotMotors.goForward()
 vikingbotMotors.set_SleepTime(1)
 vikingbotMotors.turnLeft()
@@ -23,12 +32,13 @@ vikingbotMotors.turnRight()
 vikingbotMotors.set_SleepTime(2)
 vikingbotMotors.goBack()
 
+#test for distance sensor
+#setup the distance sensor
 #ultrasonicSensorBack.setup_GPIO()
-#subprocess.call(["espeak","-s 120 -v en ", "Intoduction to robotics"] , stdout=None, stderr=subprocess.STDOUT)
+subprocess.call(["espeak","-s 120 -v en ", "Zebra is moving now"] , stdout=None, stderr=subprocess.STDOUT)
 #while(True):
-
+#if distance is more than 10 cm. go back. Ig there is an obstacle stop
 #        if (ultrasonicSensorBack.get_distance() > 10):
 #                vikingbotMotors.goBack()
-
 
 GPIO.cleanup()
